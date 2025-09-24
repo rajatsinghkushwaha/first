6c384021331020b11c11bd7ce6eba79a317ca3a9
rajatsinghkushwaha <kantakushwah123@gmail.com>
2025-09-25 01:01:09 +0530

    Add files via upload

---
+++ even or odd finder.py
@@ -0,0 +1,5 @@
+a=int(input("Please enter any whole number: "))
+if(a%2==0 or a==0):
+    print(a, "is an even number")
+else:
+    print(a, "is an odd number")
---
+++ sum of natural number.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the number of terms you want to find the sum of: "))
+sum = 0
+for x in range(1,a+1):
+    sum = sum + x
+print(sum)
+
